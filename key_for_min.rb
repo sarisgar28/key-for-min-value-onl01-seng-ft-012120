f93a720196aa41701bec428dbcbe2711bd085054
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-28 20:02:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,15 +1,1 @@
-ikea = {:chair => 25,:table => 85, :mattress => 450}
-
-def key_for_min_value(ikea)
-  #=> :chair 
-  veggies ={"apple" = |-45|,
-  "cucumber" = |-44.5,|
-   "tomato" =  |44.9| }
-   key_for_min_value(veggies)
-   def = {"apple"}
-   #sort_by(ikea)
-   puts "the lowest #{veggie} is veggie #{number}"
-  
- end
- 
-end
+def key_for_min_value ()
