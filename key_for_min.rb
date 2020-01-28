08ca92fe80e26b02af2263ef8d1727bc9ed817a3
Sara Isabel Garcia Reyes <sarisgar28@gmail.com>
2020-01-28 03:52:46 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,15 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+ikea = {:chair => 25,:table => 85, :mattress => 450}
 
-def key_for_min_value(name_hash)
-
+def key_for_min_value(ikea)
+  #=> :chair 
+  veggies ={"apple" = |-45|,
+  "cucumber" = |-44.5,|
+   "tomato" =  |44.9| }
+   key_for_min_value(veggies)
+   def = {"apple"}
+   #sort_by(ikea)
+   puts "the lowest #{veggie} is veggie #{number}"
+  
+ end
+ 
 end
